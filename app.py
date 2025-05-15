0b3356496cf59f0d92d81b8d056b8b9f5c52bd51
3dgar21 <delangeledgar34@gmail.com>
2025-05-15 18:40:43 -0400

    w

--- app.py
+++ app.py
@@ -1,50 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os, random
+import random
+import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# Spotify Embed URIs (just the embed part, not full iframe yet)
+# Sample song data: (artist, title, Spotify embed URL)
 sample_songs = {
     "Pop": [
-        "https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b",  # Blinding Lights
-        "https://open.spotify.com/embed/track/4h9wh7iOZ0GGn8QVp4RAOB",  # Levitating
-        "https://open.spotify.com/embed/track/4aWmUDTfIPGksMNLV2rQP2"   # As It Was
+        ("The Weeknd", "Blinding Lights", "https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b"),
+        ("Dua Lipa", "Levitating", "https://open.spotify.com/embed/track/463CkQjx2Zk1yXoBuierM9"),
+        ("Harry Styles", "As It Was", "https://open.spotify.com/embed/track/4LRPiXqCikLlN15c3yImP7")
     ],
     "Rock": [
-        "https://open.spotify.com/embed/track/7tFiyTwD0nx5a1eklYtX2J",  # Bohemian Rhapsody
-        "https://open.spotify.com/embed/track/5ghIJDpPoe3CfHMGu71E6T",  # Smells Like Teen Spirit
-        "https://open.spotify.com/embed/track/40riOy7x9W7GXjyGp4pjAv"   # Hotel California
+        ("Queen", "Bohemian Rhapsody", "https://open.spotify.com/embed/track/7tFiyTwD0nx5a1eklYtX2J"),
+        ("Nirvana", "Smells Like Teen Spirit", "https://open.spotify.com/embed/track/5ghIJDpPoe3CfHMGu71E6T"),
+        ("Eagles", "Hotel California", "https://open.spotify.com/embed/track/40riOy7x9W7GXjyGp4pjAv")
     ],
-    "Hip-hop": [
-        "https://open.spotify.com/embed/track/2xLMifQCjDGFmkHkpNLD9h",  # SICKO MODE
-        "https://open.spotify.com/embed/track/3qT4bUD1MaWpGrTwcvguhb",  # God's Plan
-        "https://open.spotify.com/embed/track/2dLLR6qlu5UJ5gk0dKz0h3"   # Money Trees
+    "Hip-Hop": [
+        ("Travis Scott", "SICKO MODE", "https://open.spotify.com/embed/track/2xLMifQCjDGFmkHkpNLD9h"),
+        ("Drake", "God's Plan", "https://open.spotify.com/embed/track/6DCZcSspjsKoFjzjrWoCdn"),
+        ("Kendrick Lamar", "Money Trees", "https://open.spotify.com/embed/track/2vwlzO0Qp8kfEtzTsCXfyE")
     ],
     "R&B": [
-        "https://open.spotify.com/embed/track/2BtE7qm1qzM80p9vLSiXkj",  # We Belong Together
-        "https://open.spotify.com/embed/track/4RCwb5c5zQfQVsGiFOy1cM",  # Blame It
-        "https://open.spotify.com/embed/track/4w8niZpiMy6qz1mntFA5uM"   # Adorn
+        ("Mariah Carey", "We Belong Together", "https://open.spotify.com/embed/track/4pbJqGIASGPr0ZpGpnWkDn"),
+        ("Jamie Foxx", "Blame It", "https://open.spotify.com/embed/track/0xYTqQeNBMZ5tPjUOJF1EC"),
+        ("Miguel", "Adorn", "https://open.spotify.com/embed/track/0iM1M9CzZkLXEfpKzRddHI")
     ],
     "Norteñas": [
-        "https://open.spotify.com/embed/track/1WkMMavIMc4x3bpaIw74v2",  # LADY GAGA
-        "https://open.spotify.com/embed/track/1z3HgkzqdeMPv9zM7EZGdh",  # AMG
-        "https://open.spotify.com/embed/track/7MAibcTli4IisCtbHKrGMh"   # PRC
+        ("Peso Pluma", "LADY GAGA", "https://open.spotify.com/embed/track/0fZKkcrbZk4WkOrcFJbrZT"),
+        ("Natanael Cano", "AMG", "https://open.spotify.com/embed/track/5odlY52u43F5BjByhxg7wg"),
+        ("Peso Pluma", "PRC", "https://open.spotify.com/embed/track/6UjfByV0nHNs5WJvTPAsvm")
     ]
 }
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        selected_genres = request.form.getlist("genres")
+        if not selected_genres:
+            return redirect(url_for("home"))
+
+        recs = []
+        for genre in selected_genres:
+            if genre in sample_songs:
+                recs.extend(random.sample(sample_songs[genre], min(1, len(sample_songs[genre]))))
+
+        return render_template("result.html", recs=recs)
+
     return render_template("index.html")
 
-@app.route("/recommend", methods=["POST"])
-def recommend():
-    genre = request.form.get("genre")
-    if not genre or genre not in sample_songs:
-        return redirect(url_for("home"))
-
-    recommended = random.sample(sample_songs[genre], min(3, len(sample_songs[genre])))
-    return render_template("result.html", songs=recommended)
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
